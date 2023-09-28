994e570ebdd1899cd855cc6f9e1dc4e2df89b3a0
Ronaldo <ronaldofidelis.ti@gmail.com>
2023-09-27 23:47:22 -0300

    Feat: AdjsutComponentsMainAside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,23 +1,24 @@
 import Icon from '../../public/icon/music.svg'
-import {Home, Search, Library, Plus, ChevronDown, ArrowRight} from 'lucide-react'
+import { Home, Search, Library, Plus, ChevronDown, ArrowRight } from 'lucide-react'
 
 const musics = [
-  { name: 'Músicas Curtidas',
+  {
+    name: 'Músicas Curtidas',
     icon: Icon,
     playlist: 'Favoritas',
-  },{
+  }, {
     name: 'Funks Hits',
     icon: Icon,
     playlist: 'Hits',
-  },{
+  }, {
     name: 'MPB',
     icon: Icon,
     playlist: 'mais tocadas',
-  },{
+  }, {
     name: 'Frank Sinatra',
     icon: Icon,
     playlist: 'Artista',
-  },{
+  }, {
     name: 'Lil Whind',
     icon: Icon,
     playlist: 'Artista',
@@ -27,8 +28,8 @@ function Aside() {
 
   return (
     <div className='flex flex-col items-center justify-center gap-2'>
-      <aside className='w-96 p-6 h-1/5 flex items-center rounded-lg bg-aside'>
-        <div className='flex flex-col gap-6'>
+      <aside className='w-96 p-2 h-1/5 flex items-center rounded-lg bg-aside'>
+        <div className='m-4 flex flex-col gap-6'>
 
           <a href="#"
             className='flex flex-row items-center gap-5 text-base font-bold hover:text-white duration-500'>
@@ -45,58 +46,58 @@ function Aside() {
         </div>
       </aside>
 
-      <aside className='w-96 p-6 h-4/5 rounded-lg bg-aside'>
-
-        <div className='flex items-center justify-between'>
-          <a href="#"
-            className='flex flex-row items-center gap-2 text-base font-bold hover:text-white duration-500'>
-            <Library />
-            Sua Biblioteca
-          </a>
-
-          <div className='flex gap-1 items-center justify-center'>
-
+      <aside className='w-96 h-4/5 rounded-lg bg-aside p-2'>
+        <div className='flex flex-col justify-center shadow-md'>
+          <div className='flex items-center justify-between m-4'>
             <a href="#"
-              className='hover:bg-zinc-800 rounded-full duration-300 p-1'>
-              <Plus />
+              className='flex flex-row items-center gap-2 text-base font-bold hover:text-white duration-500'>
+              <Library />
+              Sua Biblioteca
             </a>
 
-            <a href="#"
-              className='hover:bg-zinc-800 rounded-full duration-300 p-1'>
-              <ArrowRight />
-            </a>
+            <div className='flex gap-1 items-center justify-center'>
 
+              <a href="#"
+                className='hover:bg-zinc-800 rounded-full duration-300 p-1'>
+                <Plus />
+              </a>
+
+              <a href="#"
+                className='hover:bg-zinc-800 rounded-full duration-300 p-1'>
+                <ArrowRight />
+              </a>
+            </div>
+          </div>
+          <div className='flex flex-nowrap items-center mx-4 mb-2 gap-1 overflow-x-hidden max-w-screen-lg'>
+            <a href="#"
+              className='flex items-center whitespace-nowrap bg-zinc-900 p-2 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
+              Playlists</a>
+            <a href="#"
+              className='flex items-center whitespace-nowrap bg-zinc-900 p-2 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
+              Artistas</a>
+            <a href="#"
+              className='flex items-center whitespace-nowrap bg-zinc-900 p-2 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
+              Álbuns</a>
+            <a href="#"
+              className='flex items-center whitespace-nowrap bg-zinc-900 p-2 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
+              Podcasts e programas</a>
+          </div>
+          <div className='flex items-center justify-between mx-4 mb-2 '>
+            <a href="#"
+              className='flex items-center hover:bg-zinc-800 rounded-full duration-300 p-1'>
+              <Search /></a>
+            <a href="#"
+              className='flex items-center text-sm font-semibold'>
+              Recentes<ChevronDown/></a>
           </div>
         </div>
 
-        <div className='flex flex-row flex-nowrap gap-3 overflow-x-scroll m-2 p-1'>
-          <a href="#"
-            className='flex items-center justify-center whitespace-nowrap bg-zinc-900 p-3 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
-            Playlists</a>
-          <a href="#"
-            className='flex items-center justify-center whitespace-nowrap bg-zinc-900 p-3 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
-            Artistas</a>
-          <a href="#"
-            className='flex items-center justify-center whitespace-nowrap bg-zinc-900 p-3 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
-            Álbuns</a>
-          <a href="#"
-            className='flex items-center justify-center whitespace-nowrap bg-zinc-900 p-3 rounded-full text-white text-xs font-medium hover:bg-zinc-800 duration-500' >
-            Podcasts e programas</a>
-        </div>
+        <div className='h-72 max-w-screen-lg flex flex-col overflow-y-hidden hover:overflow-y-scroll gap-2'>
 
-        <div className='flex justify-between items-center mb-2'>
-          <a href="#"
-            className='flex items-center justify-center hover:bg-zinc-800 rounded-full duration-300 p-1'>
-            <Search /></a>
-          <a href="#"
-            className='flex items-center justify-center text-sm font-semibold' >
-            Recentes<ChevronDown /></a>
-        </div>
 
-        <nav className='h-64 flex flex-col flex-nowrap overflow-y-hidden hover:overflow-y-scroll gap-2'>
           {musics.map((musica, id) => {
             return (
-              <div key={id} className='flex items-center p-1 gap-1 rounded-lg cursor-pointer hover:bg-zinc-800'>
+              <div key={id} className='flex items-center p-1 gap-1 rounded-lg cursor-pointer hover:bg-zinc-800 mx-4'>
                 <img src={musica.icon} className='w-14' alt="music" />
                 <div className='flex flex-col'>
                   <h1 className='text-base font-bold text-white'>{musica.name}</h1>
@@ -105,7 +106,7 @@ function Aside() {
               </div>
             )
           })}
-        </nav>
+        </div>
       </aside>
     </div>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,29 +6,25 @@ function Main() {
 
   return (
     <main className="flex flex-col flex-1 p-6 rounded-lg bg-main">
-      <header className='mb-5'>
-        <nav className='sticky top-0 min-w-full'>
+      <nav className='sticky top-0 min-w-full shadow-md px-2'>
           <div className='flex flex-row items-center justify-between'>
             <div className='flex items-center gap-5'>
-              <a href="#" className='flex items-center justify-center bg-card rounded-full p-1 mx-auto'><ChevronLeft /></a>
-              <a href="#" className='flex items-center justify-center bg-card rounded-full p-1 mx-auto'><ChevronRight /></a>
+              <a href="#" className='flex items-center justify-center hover:bg-card duration-500 rounded-full p-1 mx-auto'><ChevronLeft /></a>
+              <a href="#" className='flex items-center justify-center hover:bg-card duration-500 rounded-full p-1 mx-auto'><ChevronRight /></a>
             </div>
-            <div className='flex items-center justify-center flex-row gap-2'>
-              <a href="#" className='flex items-center justify-center overflow-hidden text-xs px-3 py-2 font-bold bg-white text-black rounded-full'>Ver planos Premium</a>
-              <a href="#" className='flex items-center justify-center overflow-hidden text-xs px-3 py-2 font-bold bg-card text-white rounded-full gap-2'>Instalar aplicativo</a>
-              <a href="#" className='flex items-center justify-center overflow-hidden bg-card text-white rounded-full p-2'><Bell className='w-4 h-4' /></a>
-              <a href="#" className='flex items-center justify-center overflow-hidden bg-white text-white rounded-full p-1'>
+            <div className='flex items-center justify-center flex-row gap-2 mb-2'>
+              <a href="#" className='flex items-center justify-center overflow-hidden text-xs px-3 py-2 font-bold bg-white text-black rounded-full mb-2'>Ver planos Premium</a>
+              <a href="#" className='flex items-center justify-center overflow-hidden text-xs px-3 py-2 font-bold bg-card text-white rounded-full gap-2 mb-2'>Instalar aplicativo</a>
+              <a href="#" className='flex items-center justify-center overflow-hidden bg-card text-white rounded-full p-2 mb-2'><Bell className='w-4 h-4' /></a>
+              <a href="#" className='flex items-center justify-center overflow-hidden bg-white text-white rounded-full p-1 mb-2'>
                 <img className='w-6' src={iconUser} alt="" />
               </a>
             </div>
           </div>
         </nav>
-      </header>
-      <div className='h-main-card overflow-y-hidden hover:overflow-y-scroll'>
+      <div className='h-main-card max-w-screen-lg overflow-y-hidden hover:overflow-y-scroll'>
         <HighlightsNight />
-        <HighlightsNight />
-        <HighlightsNight />
-        <HighlightsNight />
+        
       </div>
     </main>
   )
